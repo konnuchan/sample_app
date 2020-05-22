17376a20a1a2cd164b56cbb73453827143909884
Satoko Mochizuki <konnuchan@gmail.com>
2020-05-22 09:24:34 +0000

    finish ch13

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,3 +7,13 @@ class ApplicationController < ActionController::Base
   end
 end
 
+private
+  
+# ログイン済みユーザーかどうか確認
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
